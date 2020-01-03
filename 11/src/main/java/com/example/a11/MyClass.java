4a736fbd9cb74f91ae2084a2d4b8baac986a882a
love190love <love190love@yahoo.com.tw>
2020-01-03 14:56:57 +0800

    Initial commit

---
+++ 11/src/main/java/com/example/a11/MyClass.java
@@ -0,0 +1,11 @@
+package com.example.a11;
+
+
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String[] args) {
+        PrizeNumber prizeNumber = new PrizeNumber("7_8.txt");
+        prizeNumber.Judge();
+    }
+}
